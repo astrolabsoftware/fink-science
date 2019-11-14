6f331215e42d7641cd25e518a33183131a67b082
Julien <peloton@lal.in2p3.fr>
2019-11-14 22:23:51 +0100

    Fix typo

--- setup.py
+++ setup.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import setuptools
-import fink_broker
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
