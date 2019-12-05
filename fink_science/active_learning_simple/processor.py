34326ce3d831303f99c6c4a31d329dfde6e78563
Julien <peloton@lal.in2p3.fr>
2019-12-05 14:29:14 +0100

    initial push of the active learning module: simple bazin fit and Random Forest Classifier

---
+++ fink_science/active_learning_simple/processor.py
@@ -0,0 +1,68 @@
+# Copyright 2019 AstroLab Software
+# Author: Julien Peloton
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from pyspark.sql.functions import pandas_udf, PandasUDFType
+from pyspark.sql.types import BooleanType, DoubleType
+
+import pandas as pd
+import numpy as np
+
+from fink_science.active_learning_simple.classifier import apply_classifier
+from fink_science.tester import spark_unit_tests
+
+from typing import Any
+
+@pandas_udf(DoubleType(), PandasUDFType.SCALAR)
+def iaclassification(time, mag, band, htimes, hmags, hbands) -> pd.Series:
+    """ Return the probability of an alert to be a SN1a using a Random
+    Forest Classifier.
+
+    Parameters
+    ----------
+    time: Spark DataFrame Column
+        Column of candidate time (float): candidate.jd
+    mag: Spark DataFrame Column
+        Column of candidate flux (float): candidate.magpsf
+    band: Spark DataFrame Column
+        Column of candidate filter ID (int): candidate.fid
+    htimes: Spark DataFrame Column
+        Column of historical time vectors: prv_candidates.jd
+    hmags: Spark DataFrame Column
+        Column of historical flux vectors: prv_candidates.magpsf
+    hbands: Spark DataFrame Column
+        Column of historical filter ID vectors: prv_candidates.fid
+    """
+    alltimes = [[i] + j for i, j in zip(time, htimes)]
+    allmags = [[i] + j for i, j in zip(mag, hmags)]
+    allbands = [[i] + j for i, j in zip(band, hbands)]
+
+    # alltimes = [i[::-1] for i in alltimes]
+    # allmags = [i[::-1] for i in allmags]
+    # allbands = [i[::-1] for i in allbands]
+
+    predictions, probabilities = apply_classifier(
+        alltimes, allmags, allbands
+    )
+
+    # print(predictions, probabilities)
+
+    # Check the type of prob
+    return pd.Series(probabilities.T[0])
+
+
+if __name__ == "__main__":
+    """ Execute the test suite """
+
+    # Run the test suite
+    spark_unit_tests(globals())
