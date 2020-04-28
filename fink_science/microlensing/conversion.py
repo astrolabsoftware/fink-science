4bb62cbcc054da3e4d5c068526bf258e46810370
Julien <peloton@lal.in2p3.fr>
2020-04-28 09:56:34 +0200

    Initial commit for the microlensing science module

---
+++ fink_science/microlensing/conversion.py
@@ -0,0 +1,75 @@
+# Copyright 2020 AstroLab Software
+# Author: Julien Peloton
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import numpy as np
+
+def dc_mag(fid, magpsf, sigmapsf, magnr, sigmagnr, magzpsci, isdiffpos):
+    """ Compute apparent magnitude from difference magnitude supplied by ZTF
+    Parameters
+
+    Stolen from Lasair.
+
+    ----------
+    fid
+        filter, 1 for green and 2 for red
+    magpsf,sigmapsf
+        magnitude from PSF-fit photometry, and 1-sigma error
+    magnr,sigmagnr
+        magnitude of nearest source in reference image PSF-catalog
+        within 30 arcsec and 1-sigma error
+    magzpsci
+        Magnitude zero point for photometry estimates
+    isdiffpos
+        t or 1 => candidate is from positive (sci minus ref) subtraction;
+        f or 0 => candidate is from negative (ref minus sci) subtraction
+    """
+
+    # zero points. Looks like they are fixed.
+    ref_zps = {1: 26.325, 2: 26.275, 3: 25.660}
+    magzpref = ref_zps[fid]
+
+    # reference flux and its error
+    magdiff = magzpref - magnr
+    if magdiff > 12.0:
+        magdiff = 12.0
+    ref_flux = 10**(0.4 * magdiff)
+    ref_sigflux = (sigmagnr / 1.0857) * ref_flux
+
+    # difference flux and its error
+    if magzpsci is None:
+        magzpsci = magzpref
+    magdiff = magzpsci - magpsf
+    if magdiff > 12.0:
+        magdiff = 12.0
+    difference_flux = 10**(0.4 * magdiff)
+    difference_sigflux = (sigmapsf / 1.0857) * difference_flux
+
+    # add or subract difference flux based on isdiffpos
+    if isdiffpos == 't':
+        dc_flux = ref_flux + difference_flux
+    else:
+        dc_flux = ref_flux - difference_flux
+
+    # assumes errors are independent. Maybe too conservative.
+    dc_sigflux = np.sqrt(difference_sigflux**2 + ref_sigflux**2)
+
+    # apparent mag and its error from fluxes
+    if (dc_flux == dc_flux) and dc_flux > 0.0:
+        dc_mag = magzpsci - 2.5 * np.log10(dc_flux)
+        dc_sigmag = dc_sigflux / dc_flux * 1.0857
+    else:
+        dc_mag = magzpsci
+        dc_sigmag = sigmapsf
+
+    return dc_mag, dc_sigmag
