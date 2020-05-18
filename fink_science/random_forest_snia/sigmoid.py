e5602ec084fb4a279015c8b7066f2a3a95da6f35
Julien <peloton@lal.in2p3.fr>
2020-05-18 10:28:49 +0200

    Fix wrong imports from the refactoring

--- fink_science/random_forest_snia/sigmoid.py
+++ fink_science/random_forest_snia/sigmoid.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import numpy as np
+import pandas as pd
 
 from scipy.optimize import least_squares
 from scipy.stats import chisquare
