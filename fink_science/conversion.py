95fb8a057c881f5f1f905d9a6040991f6c6265ef
Julien <peloton@lal.in2p3.fr>
2020-06-04 15:01:30 +0200

    Add new conversion routine for SNANA-based simulations (#33)
    * add new conversion routine for SNANA-based simulations
    
    * update the test suite with new arguments
    
    * Remove trailing mask...
    
    * Update test with minimum value
    
    * Update the random forest classifier with new conversion routine based on snana units
    
    * remove unused code
    
    * Bring back used conversion routine for microlensing

--- fink_science/conversion.py
+++ fink_science/conversion.py
@@ -14,130 +14,34 @@
 # limitations under the License.
 import numpy as np
 
-def mag2flux(fid, magpsf, sigmapsf, magnr, sigmagnr, magzpsci, isdiffpos):
-    """ Compute flux from difference magnitude supplied by ZTF.
-
-    Vectorized version of `dc_flux`
+def mag2fluxcal_snana(magpsf: float, sigmapsf: float):
+    """ Conversion from magnitude to Fluxcal from SNANA manual
 
     Parameters
     ----------
-    fid: int
-        filter, 1 for green and 2 for red
-    magpsf, sigmapsf: 1D arrays of float
-        magnitude from PSF-fit photometry, and 1-sigma error
-    magnr, sigmagnr: 1D arrays of float
-        magnitude of nearest source in reference image PSF-catalog
-        within 30 arcsec and 1-sigma error
-    magzpsci: 1D array of float
-        Magnitude zero point for photometry estimates
-    isdiffpos: 1D array of string
-        t => candidate is from positive (sci minus ref) subtraction
-        f => candidate is from negative (ref minus sci) subtraction
+    magpsf: float
+        PSF-fit magnitude from ZTF
+    sigmapsf: float
 
     Returns
-    ---------
-    dc_flux, dc_sigflux: flux and its error as arrays
-    """
-    # Zero points. Looks like they are fixed.
-    # Where this information is stored? How reliable over time it is?
-    ref_zps = {1: 26.325, 2: 26.275, 3: 25.660}
-    magzpref = ref_zps[fid]
+    ----------
+    fluxcal: float
+        Flux cal as used by SNANA
+    fluxcal_err: float
+        Absolute error on fluxcal (the derivative has a minus sign)
 
-    # reference flux and its error
-    magdiff = magzpref - magnr
-    mask = magdiff > 12
-    magdiff[mask] = 12.0
-    ref_flux = 10**(0.4 * (magdiff))
-    ref_sigflux = (sigmagnr / 1.0857) * ref_flux
-
-    # difference flux and its error
-    mask = magzpsci == 0
-    magzpsci[mask] = magzpref
-    magdiff = magzpsci - magpsf
-    mask = magdiff > 12
-    magdiff[mask] = 12.0
-    difference_flux = 10**(0.4 * (magdiff))
-    difference_sigflux = (sigmapsf / 1.0857) * difference_flux
-
-    # add or subract difference flux based on isdiffpos
-    mask = isdiffpos == 't'
-    dc_flux = np.zeros_like(ref_flux)
-    dc_flux[mask] = ref_flux[mask] + difference_flux[mask]
-    dc_flux[~mask] = ref_flux[~mask] - difference_flux[~mask]
-
-    # assumes errors are independent. Maybe too conservative.
-    dc_sigflux = np.sqrt(difference_sigflux**2 + ref_sigflux**2)
-
-    return dc_flux, dc_sigflux
-
-
-def dc_flux(fid, magpsf, sigmapsf, magnr, sigmagnr, magzpsci, isdiffpos):
-    """ Compute apparent flux from difference magnitude supplied by ZTF
-
-    This was heavily influenced by the computation provided by Lasair:
-    https://github.com/lsst-uk/lasair/blob/master/src/alert_stream_ztf/common/mag.py
-
-    Paramters
-    ---------
-    fid
-        filter, 1 for green and 2 for red
-    magpsf,sigmapsf; floats
-        magnitude from PSF-fit photometry, and 1-sigma error
-    magnr,sigmagnr: floats
-        magnitude of nearest source in reference image PSF-catalog
-        within 30 arcsec and 1-sigma error
-    magzpsci: float
-        Magnitude zero point for photometry estimates
-    isdiffpos: str
-        t or 1 => candidate is from positive (sci minus ref) subtraction;
-        f or 0 => candidate is from negative (ref minus sci) subtraction
-
-    Returns
-    --------
-    dc_flux: float
-        Apparent magnitude
-    dc_sigflux: float
-        Error on apparent magnitude
     """
     if magpsf is None:
         return None, None
-    # zero points. Looks like they are fixed.
-    ref_zps = {1: 26.325, 2: 26.275, 3: 25.660}
-    magzpref = ref_zps[fid]
+    fluxcal = 10 ** (-0.4 * magpsf) * 10 ** (11)
+    fluxcal_err = 9.21034 * 10 ** 10 * np.exp(-0.921034 * magpsf) * sigmapsf
 
-    # reference flux and its error
-    magdiff = magzpref - magnr
-    if magdiff > 12.0:
-        magdiff = 12.0
-    ref_flux = 10**(0.4 * magdiff)
-    ref_sigflux = (sigmagnr / 1.0857) * ref_flux
-
-    # difference flux and its error
-    if magzpsci == 0.0:
-        magzpsci = magzpref
-    magdiff = magzpsci - magpsf
-    if magdiff > 12.0:
-        magdiff = 12.0
-    difference_flux = 10**(0.4 * magdiff)
-    difference_sigflux = (sigmapsf / 1.0857) * difference_flux
-
-    # add or subract difference flux based on isdiffpos
-    if isdiffpos == 't':
-        dc_flux = ref_flux + difference_flux
-    else:
-        dc_flux = ref_flux - difference_flux
-
-    # assumes errors are independent. Maybe too conservative.
-    dc_sigflux = np.sqrt(difference_sigflux**2 + ref_sigflux**2)
-
-    return dc_flux, dc_sigflux
+    return fluxcal, fluxcal_err
 
 def dc_mag(fid, magpsf, sigmapsf, magnr, sigmagnr, magzpsci, isdiffpos):
     """ Compute apparent magnitude from difference magnitude supplied by ZTF
     Parameters
-
     Stolen from Lasair.
-
     ----------
     fid
         filter, 1 for green and 2 for red
