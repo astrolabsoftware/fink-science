3e31a6e00935b16fa3c2542dd4abea9d4d5118ce
Julien <peloton@lal.in2p3.fr>
2021-03-15 08:14:18 +0100

    Add linter in GitHub Actions

--- fink_science/conversion.py
+++ fink_science/conversion.py
@@ -1,4 +1,4 @@
-# Copyright 2019 AstroLab Software
+# Copyright 2019-2021 AstroLab Software
 # Author: Julien Peloton
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
--- fink_science/tester.py
+++ fink_science/tester.py
@@ -1,4 +1,4 @@
-# Copyright 2019 AstroLab Software
+# Copyright 2019-2021 AstroLab Software
 # Author: Julien Peloton
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import os
 import sys
 import doctest
 import numpy as np
