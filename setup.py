astrolabsoftware/fink-science

commit b60958e2a0ef994f3242c9d28cbc201f2a173bc1

Add *csv files in setup.py (#100)
This will enable the file components.csv from data/models to be loaded by kilonova.processor.knscore.

setup.py
@@ -35,6 +35,7 @@ setuptools.setup(
             'data/models/*.sav',
             'data/models/*.npy',
             'data/models/*.pkl',
+            'data/models/*.csv'
             'data/models/snn_models/*/*.pt',
             'data/models/snn_models/*/*.json',
             'data/models/snn_models/*/*.txt'],

commit 7d7de26a522152fe9bc4d6be4c392cf6ae6260b2

[HOTFIX] fix typo introduced in #100 (missing comma)

setup.py
@@ -35,7 +35,7 @@ setuptools.setup(
             'data/models/*.sav',
             'data/models/*.npy',
             'data/models/*.pkl',
-            'data/models/*.csv'
+            'data/models/*.csv',
             'data/models/snn_models/*/*.pt',
             'data/models/snn_models/*/*.json',
             'data/models/snn_models/*/*.txt'],