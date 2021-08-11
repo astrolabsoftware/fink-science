2b5d281b1ecbffc2474d93592dfc62d5e3846dd0
Julien <peloton@lal.in2p3.fr>
2021-08-11 09:07:53 +0200

    add documentation to the script to decode external catalogs

--- fink_science/data/catalogs/decode_gcvs.py
+++ fink_science/data/catalogs/decode_gcvs.py
@@ -1,3 +1,21 @@
+# Copyright 2021 AstroLab Software
+# Author: Julien Peloton
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+""" Decode and format GCVS and VSX catalogs.
+Download GCVS catalog (http://sai.msu.su/gcvs/gcvs/) as txt file, and
+VSX from Vizier as tsv file, and simply run `python decode_catalogs.py`
+"""
 import io
 import numpy as np
 import pandas as pd
