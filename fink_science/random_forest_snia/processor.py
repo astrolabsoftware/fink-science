8843d5a359735bd10a8d7eada5b2f4199636fc4b
Julien <peloton@lal.in2p3.fr>
2022-03-24 10:57:43 +0100

    Update tests accordingly

--- fink_science/random_forest_snia/processor.py
+++ fink_science/random_forest_snia/processor.py
@@ -86,7 +86,7 @@ def rfscore_sigmoid_full(jd, fid, magpsf, sigmapsf, cdsxmatch, ndethist, model=N
     >>> df = df.withColumn('pIa', rfscore_sigmoid_full(*args))
 
     >>> df.filter(df['pIa'] > 0.5).count()
-    5
+    6
 
     >>> df.filter(df['pIa'] > 0.5).select(['rf_snia_vs_nonia', 'pIa']).show()
     +----------------+-----+
