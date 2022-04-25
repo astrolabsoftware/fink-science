0760f889a7b1cbd2724dca138050aa8086471765
Julien <peloton@lal.in2p3.fr>
2022-04-25 20:26:42 +0200

    PEP8 fix

--- fink_science/t2/utilities.py
+++ fink_science/t2/utilities.py
@@ -13,6 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import numpy as np
+import pandas as pd
 
 from tensorflow import keras
 from pathlib import Path
