89d972fbca41d86af695721bb6e21ec829941de9
erusseil <etiennerusseil63@gmail.com>
2022-06-09 14:10:53 +0200

    Add example agn

--- fink_science/agn/processor.py
+++ fink_science/agn/processor.py
@@ -7,6 +7,14 @@ import pandas as pd
 @pandas_udf(DoubleType())
 def agn_spark(objectId, jd, magpsf, sigmapsf, fid):
     
+    """
+    Examples
+    --------
+    >>> df = spark.read.format('parquet').load(ztf_alert_sample)
+    >>> df.show()
+   
+    """
+    
     data = pd.DataFrame({
         'objectId': objectId,
         'cjd':jd,
@@ -17,3 +25,15 @@ def agn_spark(objectId, jd, magpsf, sigmapsf, fid):
     
     proba = agn_classifier(data)
     return pd.Series(proba)
+
+
+if __name__=='__main__':
+    
+    globs = globals()
+    path = os.path.dirname(__file__)
+
+    ztf_alert_sample = 'file://{}/data/alerts/agn_example.parquet'.format(path)
+    globs["ztf_alert_sample"] = ztf_alert_sample
+
+    # Run the test suite
+    spark_unit_tests(globs)
