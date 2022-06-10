6f407ac4b32cb747ee75aacd4eead4ecfc6257e8
Julien <peloton@lal.in2p3.fr>
2022-06-10 11:07:37 +0200

    Force test suite to continue

--- fink_science/snn/processor.py
+++ fink_science/snn/processor.py
@@ -146,7 +146,7 @@ def snn_ia(candid, jd, fid, magpsf, sigmapsf, roid, cdsxmatch, jdstarthist, mode
     >>> df = df.withColumn('pIa', snn_ia(*args))
 
     >>> df.filter(df['pIa'] > 0.5).count()
-    6
+    7
     """
     mask = apply_selection_cuts_ztf(magpsf, cdsxmatch, jd, jdstarthist, roid)
 
