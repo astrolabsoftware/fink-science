3e387a2ff1a08381fa0f1438bfe3c9a2bd761493
erusseil <etiennerusseil63@gmail.com>
2022-06-10 15:46:50 +0200

    Added examples

--- fink_science/agn/models.py
+++ fink_science/agn/models.py
@@ -2,13 +2,27 @@ import numpy as np
 
 
 def protected_exponent(x1):
-    """Exponential function : cannot exceed e**10"""
-    with np.errstate(over="ignore"):
-        return np.where(np.abs(x1) < 10, np.exp(x1), np.exp(10))
+    """Exponential function : cannot exceed e**10
+    
+    Examples
+    --------
+    >>> np.round_(protected_exponent(42), 5)
+    22026.46579
+    >>> np.round_(protected_exponent(1), 5)
+    2.71828
+    """
+    return np.where(np.abs(x1) < 10, np.exp(x1), np.exp(10))
 
 
 def sig(x):
-    """Sigmoid function using the protected exponential function"""
+    """Sigmoid function using the protected exponential function
+    
+    Examples
+    --------
+    >>> sig(0)
+    0.5
+    """
+    
     return 1 / (1 + protected_exponent(-x))
 
 
@@ -26,6 +40,18 @@ def bump(x, p1, p2, p3, p4):
     -------
     np.array
         Flux array
+        
+    Examples
+    --------    
+    >>> np.round_(bump(0, 0.225,-2.5,0.038,0), 5)
+    0.02931
     """
 
     return sig(p1 * x + p2 - protected_exponent(p3 * x)) + p4
+
+if __name__ == "__main__":
+    
+    import sys
+    import doctest
+    
+    sys.exit(doctest.testmod()[0])
