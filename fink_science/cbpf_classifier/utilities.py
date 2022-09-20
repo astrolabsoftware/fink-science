16d7b1341c4216408af05e208095a47d5ca86e15
André Santos <72099036+and-santos@users.noreply.github.com>
2022-09-20 17:38:26 -0300

    CBPF Classifier Integration (#200)
    * First commit cbpf classifier
    
    * Initial setup for cbpf classifier
    
    * New Changes on cbpf classifier, need test with notebooks
    
    * Change the model to a new one
    
    * omiting python notebooks
    
    * update
    
    * First (almost) working commit
    
    * Final changes on cbpf classifier
    
    * Correction on what should be on commit
    
    * more corrections on commit
    
    * Fixes on processor.py from cbpf_classifier
    
    * Fixing typo
    
    * Fixing typo
    
    * Corrections according to PEP8
    
    * Inserting new model
    
    * Fixing return

---
+++ fink_science/cbpf_classifier/utilities.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+
+def normalize_lc(lc_array: np.array) -> np.array:
+    """
+    Normalize a given array of light curves.
+
+    Parameters:
+    ----------
+    lc_array: np.array[float]
+        Input light curve of an alert.
+
+    Returns:
+    --------
+    result: np.array[float]
+        normalized light curve of an alert.
+    """
+
+    result = np.zeros((lc_array.shape[0], 3))
+    result[:, 0] = lc_array[:, 0]
+    result[:, 0] -= lc_array[0, 0]
+    for i in range(1, 3):
+        norm = (lc_array[:, i] - np.min(lc_array[:, i])) / \
+            np.ptp(lc_array[:, i])
+        result[:, i] = norm
+
+    return result
