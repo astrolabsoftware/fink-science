9eec4f7aa6aa0eb7aba67069418189b38e7e0bfa
JulienPeloton <peloton@lal.in2p3.fr>
2022-09-23 13:49:57 +0200

    Update the test for EarlySN to make it discriminant between models

--- fink_science/random_forest_snia/processor.py
+++ fink_science/random_forest_snia/processor.py
@@ -331,7 +331,7 @@ def rfscore_sigmoid_elasticc(midPointTai, filterName, psFlux, psFluxErr, cdsxmat
     >>> args += [F.col('cdsxmatch'), F.col('diaSource.nobs')]
     >>> df = df.withColumn('pIa', rfscore_sigmoid_elasticc(*args))
 
-    >>> df.filter(df['pIa'] > 0.0).count()
+    >>> df.filter(df['pIa'] > 0.5).count()
     29
     """
     mask = apply_selection_cuts_ztf(psFlux, nobs, cdsxmatch, maxndethist=100)
