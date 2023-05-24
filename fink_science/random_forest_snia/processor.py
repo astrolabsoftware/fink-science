206afc72f5de8d19e81d0cbd03cb4b887742f321
marcol <marcol@marcols-MacBook-Pro.local>
2023-05-24 15:16:58 +0200

    fix merge

--- fink_science/random_forest_snia/processor.py
+++ fink_science/random_forest_snia/processor.py
@@ -40,254 +40,246 @@ def apply_selection_cuts_ztf(
     for early SN Ia analysis
 
     Parameters
-    ----------
-    magpsf: pd.Series
-        Series containing data measurement (array of double). Each row contains
-        all measurement values for one alert.
-    ndethist: pd.Series
-        Series containing length of the alert history (int).
-        Each row contains the (single) length of the alert.
-    cdsxmatch: pd.Series
-        Series containing crossmatch label with SIMBAD (str).
-        Each row contains one label.
+----------
+magpsf: pd.Series
+Series containing data measurement (array of double). Each row contains
+all measurement values for one alert.
+ndethist: pd.Series
+Series containing length of the alert history (int).
+Each row contains the (single) length of the alert.
+cdsxmatch: pd.Series
+Series containing crossmatch label with SIMBAD (str).
+Each row contains one label.
 
-    Returns
-    ---------
-    mask: pd.Series
-        Series containing `True` if the alert is valid, `False` otherwise.
-        Each row contains one boolean.
-    """
-    # Flag alerts with less than 3 points in total
-    mask = magpsf.apply(lambda x: np.sum(np.array(x) == np.array(x))) >= minpoints
+Returns
+---------
+mask: pd.Series
+Series containing `True` if the alert is valid, `False` otherwise.
+Each row contains one boolean.
+"""
+# Flag alerts with less than 3 points in total
+mask = magpsf.apply(lambda x: np.sum(np.array(x) == np.array(x))) >= minpoints
 
-    # only alerts with less or equal than 20 measurements
-    mask *= (ndethist.astype(int) <= maxndethist)
+# only alerts with less or equal than 20 measurements
+mask *= (ndethist.astype(int) <= maxndethist)
 
-    # reject galactic objects
-    list_of_sn_host = return_list_of_eg_host()
-    mask *= cdsxmatch.apply(lambda x: x in list_of_sn_host)
+# reject galactic objects
+list_of_sn_host = return_list_of_eg_host()
+mask *= cdsxmatch.apply(lambda x: x in list_of_sn_host)
 
-    return mask
+return mask
 
 @pandas_udf(DoubleType(), PandasUDFType.SCALAR)
 def rfscore_sigmoid_full(jd, fid, magpsf, sigmapsf, cdsxmatch, ndethist, model=None) -> pd.Series:
-    """ Return the probability of an alert to be a SNe Ia using a Random
-    Forest Classifier (sigmoid fit).
+""" Return the probability of an alert to be a SNe Ia using a Random
+Forest Classifier (sigmoid fit).
 
-    You need to run the SIMBAD crossmatch before.
+You need to run the SIMBAD crossmatch before.
 
-    Parameters
-    ----------
-    jd: Spark DataFrame Column
-        JD times (vectors of floats)
-    fid: Spark DataFrame Column
-        Filter IDs (vectors of ints)
-    magpsf, sigmapsf: Spark DataFrame Columns
-        Magnitude from PSF-fit photometry, and 1-sigma error (vectors of floats)
-    cdsxmatch: Spark DataFrame Column
-        Type of object found in Simbad (string)
-    ndethist: Spark DataFrame Column
-        Column containing the number of detection by ZTF at 3 sigma (int)
-    model: Spark DataFrame Column, optional
-        Path to the trained model. Default is None, in which case the default
-        model `data/models/default-model.obj` is loaded.
+Parameters
+----------
+jd: Spark DataFrame Column
+JD times (vectors of floats)
+fid: Spark DataFrame Column
+Filter IDs (vectors of ints)
+magpsf, sigmapsf: Spark DataFrame Columns
+Magnitude from PSF-fit photometry, and 1-sigma error (vectors of floats)
+cdsxmatch: Spark DataFrame Column
+Type of object found in Simbad (string)
+ndethist: Spark DataFrame Column
+Column containing the number of detection by ZTF at 3 sigma (int)
+model: Spark DataFrame Column, optional
+Path to the trained model. Default is None, in which case the default
+model `data/models/default-model.obj` is loaded.
 
-    Returns
-    ----------
-    probabilities: 1D np.array of float
-        Probability between 0 (non-Ia) and 1 (Ia).
+Returns
+----------
+probabilities: 1D np.array of float
+Probability between 0 (non-Ia) and 1 (Ia).
 
-    Examples
-    ----------
-    >>> from fink_science.xmatch.processor import xmatch_cds
-    >>> from fink_utils.spark.utils import concat_col
-    >>> from pyspark.sql import functions as F
+Examples
+----------
+>>> from fink_science.xmatch.processor import xmatch_cds
+>>> from fink_utils.spark.utils import concat_col
+>>> from pyspark.sql import functions as F
 
-    >>> df = spark.read.load(ztf_alert_sample)
+>>> df = spark.read.load(ztf_alert_sample)
 
-    >>> df = xmatch_cds(df)
+>>> df = xmatch_cds(df)
 
-    # Required alert columns
-    >>> what = ['jd', 'fid', 'magpsf', 'sigmapsf']
+# Required alert columns
+>>> what = ['jd', 'fid', 'magpsf', 'sigmapsf']
 
-    # Use for creating temp name
-    >>> prefix = 'c'
-    >>> what_prefix = [prefix + i for i in what]
+# Use for creating temp name
+>>> prefix = 'c'
+>>> what_prefix = [prefix + i for i in what]
 
-    # Append temp columns with historical + current measurements
-    >>> for colname in what:
-    ...    df = concat_col(df, colname, prefix=prefix)
+# Append temp columns with historical + current measurements
+>>> for colname in what:
+...    df = concat_col(df, colname, prefix=prefix)
 
-    # Perform the fit + classification (default model)
-    >>> args = [F.col(i) for i in what_prefix]
-    >>> args += [F.col('cdsxmatch'), F.col('candidate.ndethist')]
-    >>> df = df.withColumn('pIa', rfscore_sigmoid_full(*args))
+# Perform the fit + classification (default model)
+>>> args = [F.col(i) for i in what_prefix]
+>>> args += [F.col('cdsxmatch'), F.col('candidate.ndethist')]
+>>> df = df.withColumn('pIa', rfscore_sigmoid_full(*args))
 
-    >>> df.filter(df['pIa'] > 0.5).count()
-    6
+>>> df.filter(df['pIa'] > 0.5).count()
+6
 
-    >>> df.filter(df['pIa'] > 0.5).select(['rf_snia_vs_nonia', 'pIa']).show()
-    +----------------+-----+
-    |rf_snia_vs_nonia|  pIa|
-    +----------------+-----+
-    |           0.839|0.689|
-    |           0.782|0.649|
-    |           0.887|0.803|
-    |           0.785|0.674|
-    |            0.88|0.816|
-    |           0.777|0.693|
-    +----------------+-----+
-    <BLANKLINE>
+>>> df.filter(df['pIa'] > 0.5).select(['rf_snia_vs_nonia', 'pIa']).show()
++----------------+-----+
+|rf_snia_vs_nonia|  pIa|
++----------------+-----+
+|           0.839|0.689|
+|           0.782|0.649|
+|           0.887|0.803|
+|           0.785|0.674|
+|            0.88|0.816|
+|           0.777|0.693|
++----------------+-----+
+<BLANKLINE>
 
-    # Note that we can also specify a model
-    >>> args = [F.col(i) for i in what_prefix]
-    >>> args += [F.col('cdsxmatch'), F.col('candidate.ndethist')]
-    >>> args += [F.lit(model_path_sigmoid)]
-    >>> df = df.withColumn('pIa', rfscore_sigmoid_full(*args))
+# Note that we can also specify a model
+>>> args = [F.col(i) for i in what_prefix]
+>>> args += [F.col('cdsxmatch'), F.col('candidate.ndethist')]
+>>> args += [F.lit(model_path_sigmoid)]
+>>> df = df.withColumn('pIa', rfscore_sigmoid_full(*args))
 
-    >>> df.filter(df['pIa'] > 0.5).count()
-    6
+>>> df.filter(df['pIa'] > 0.5).count()
+6
 
-    >>> df.agg({"pIa": "max"}).collect()[0][0] < 1.0
-    True
-    """
-    mask = apply_selection_cuts_ztf(magpsf, ndethist, cdsxmatch)
+>>> df.agg({"pIa": "max"}).collect()[0][0] < 1.0
+True
+"""
+mask = apply_selection_cuts_ztf(magpsf, ndethist, cdsxmatch)
 
-    if len(jd[mask]) == 0:
-        return pd.Series(np.zeros(len(jd), dtype=float))
+if len(jd[mask]) == 0:
+return pd.Series(np.zeros(len(jd), dtype=float))
 
-    candid = pd.Series(range(len(jd)))
-    pdf = format_data_as_snana(jd, magpsf, sigmapsf, fid, candid, mask)
+candid = pd.Series(range(len(jd)))
+pdf = format_data_as_snana(jd, magpsf, sigmapsf, fid, candid, mask)
 
-    # Load pre-trained model `clf`
-    if model is not None:
-        clf = load_scikit_model(model.values[0])
-    else:
-        curdir = os.path.dirname(os.path.abspath(__file__))
-        model = curdir + '/data/models/default-model_sigmoid.obj'
-        clf = load_scikit_model(model)
+# Load pre-trained model `clf`
+if model is not None:
+clf = load_scikit_model(model.values[0])
+else:
+curdir = os.path.dirname(os.path.abspath(__file__))
+model = curdir + '/data/models/default-model_sigmoid.obj'
+clf = load_scikit_model(model)
 
-    test_features = []
-    flag = []
-    for id in np.unique(pdf['SNID']):
-        pdf_sub = pdf[pdf['SNID'] == id]
-        features = get_sigmoid_features_dev(pdf_sub)
-        if (features[0] == 0) or (features[6] == 0):
-            flag.append(False)
-        else:
-            flag.append(True)
-        test_features.append(features)
+test_features = []
+flag = []
+for id in np.unique(pdf['SNID']):
+pdf_sub = pdf[pdf['SNID'] == id]
+features = get_sigmoid_features_dev(pdf_sub)
+if (features[0] == 0) or (features[6] == 0):
+    flag.append(False)
+else:
+    flag.append(True)
+test_features.append(features)
 
-    flag = np.array(flag, dtype=np.bool)
+flag = np.array(flag, dtype=np.bool)
 
-    # Make predictions
-    probabilities = clf.predict_proba(test_features)
-    probabilities[~flag] = 0.0
+# Make predictions
+probabilities = clf.predict_proba(test_features)
+probabilities[~flag] = 0.0
 
-    # Take only probabilities to be Ia
-    to_return = np.zeros(len(jd), dtype=float)
-    to_return[mask] = probabilities.T[1]
+# Take only probabilities to be Ia
+to_return = np.zeros(len(jd), dtype=float)
+to_return[mask] = probabilities.T[1]
 
-    return pd.Series(to_return)
+return pd.Series(to_return)
 
 @pandas_udf(StringType(), PandasUDFType.SCALAR)
 def extract_features_rf_snia(jd, fid, magpsf, sigmapsf, cdsxmatch, ndethist) -> pd.Series:
-    """ Return the features used by the RF classifier.
+""" Return the features used by the RF classifier.
 
-    There are 12 features. Order is:
-    a_g,b_g,c_g,snratio_g,chisq_g,nrise_g,
-    a_r,b_r,c_r,snratio_r,chisq_r,nrise_r
+There are 12 features. Order is:
+a_g,b_g,c_g,snratio_g,chisq_g,nrise_g,
+a_r,b_r,c_r,snratio_r,chisq_r,nrise_r
 
-    Parameters
-    ----------
-    jd: Spark DataFrame Column
-        JD times (float)
-    fid: Spark DataFrame Column
-        Filter IDs (int)
-    magpsf, sigmapsf: Spark DataFrame Columns
-        Magnitude from PSF-fit photometry, and 1-sigma error
-    cdsxmatch: Spark DataFrame Column
-        Type of object found in Simbad (string)
-    ndethist: Spark DataFrame Column
-        Column containing the number of detection by ZTF at 3 sigma (int)
+Parameters
+----------
+jd: Spark DataFrame Column
+JD times (float)
+fid: Spark DataFrame Column
+Filter IDs (int)
+magpsf, sigmapsf: Spark DataFrame Columns
+Magnitude from PSF-fit photometry, and 1-sigma error
+cdsxmatch: Spark DataFrame Column
+Type of object found in Simbad (string)
+ndethist: Spark DataFrame Column
+Column containing the number of detection by ZTF at 3 sigma (int)
 
-    Returns
-    ----------
-    features: list of str
-        List of string.
+Returns
+----------
+features: list of str
+List of string.
 
-    Examples
-    ----------
-    >>> from pyspark.sql.functions import split
-    >>> from pyspark.sql.types import FloatType
-    >>> from fink_utils.spark.utils import concat_col
-    >>> from pyspark.sql import functions as F
+Examples
+----------
+>>> from pyspark.sql.functions import split
+>>> from pyspark.sql.types import FloatType
+>>> from fink_utils.spark.utils import concat_col
+>>> from pyspark.sql import functions as F
 
-    >>> df = spark.read.load(ztf_alert_sample)
+>>> df = spark.read.load(ztf_alert_sample)
 
-    # Required alert columns
-    >>> what = ['jd', 'fid', 'magpsf', 'sigmapsf']
+# Required alert columns
+>>> what = ['jd', 'fid', 'magpsf', 'sigmapsf']
 
-    # Use for creating temp name
-    >>> prefix = 'c'
-    >>> what_prefix = [prefix + i for i in what]
+# Use for creating temp name
+>>> prefix = 'c'
+>>> what_prefix = [prefix + i for i in what]
 
-    # Append temp columns with historical + current measurements
-    >>> for colname in what:
-    ...    df = concat_col(df, colname, prefix=prefix)
+# Append temp columns with historical + current measurements
+>>> for colname in what:
+...    df = concat_col(df, colname, prefix=prefix)
 
-    # Perform the fit + classification (default model)
-    >>> args = [F.col(i) for i in what_prefix]
-    >>> args += [F.col('cdsxmatch'), F.col('candidate.ndethist')]
-    >>> df = df.withColumn('features', extract_features_rf_snia(*args))
+# Perform the fit + classification (default model)
+>>> args = [F.col(i) for i in what_prefix]
+>>> args += [F.col('cdsxmatch'), F.col('candidate.ndethist')]
+>>> df = df.withColumn('features', extract_features_rf_snia(*args))
 
-    >>> for name in RF_FEATURE_NAMES:
-    ...   index = RF_FEATURE_NAMES.index(name)
-    ...   df = df.withColumn(name, split(df['features'], ',')[index].astype(FloatType()))
+>>> for name in RF_FEATURE_NAMES:
+...   index = RF_FEATURE_NAMES.index(name)
+...   df = df.withColumn(name, split(df['features'], ',')[index].astype(FloatType()))
 
-    # Trigger something
-    >>> df.agg({RF_FEATURE_NAMES[0]: "min"}).collect()[0][0]
-    0.0
-    """
-    mask = apply_selection_cuts_ztf(magpsf, ndethist, cdsxmatch)
+# Trigger something
+>>> df.agg({RF_FEATURE_NAMES[0]: "min"}).collect()[0][0]
+0.0
+"""
+mask = apply_selection_cuts_ztf(magpsf, ndethist, cdsxmatch)
 
-    if len(jd[mask]) == 0:
-        return pd.Series(np.zeros(len(jd), dtype=float))
+if len(jd[mask]) == 0:
+return pd.Series(np.zeros(len(jd), dtype=float))
 
-    candid = pd.Series(range(len(jd)))
-    pdf = format_data_as_snana(jd, magpsf, sigmapsf, fid, candid, mask)
+candid = pd.Series(range(len(jd)))
+pdf = format_data_as_snana(jd, magpsf, sigmapsf, fid, candid, mask)
 
-    test_features = []
-    for id in np.unique(pdf['SNID']):
-        pdf_sub = pdf[pdf['SNID'] == id]
-        features = get_sigmoid_features_dev(pdf_sub)
-        test_features.append(features)
+test_features = []
+for id in np.unique(pdf['SNID']):
+pdf_sub = pdf[pdf['SNID'] == id]
+features = get_sigmoid_features_dev(pdf_sub)
+test_features.append(features)
 
-    to_return_features = np.zeros((len(jd), len(RF_FEATURE_NAMES)), dtype=float)
-    to_return_features[mask] = test_features
+to_return_features = np.zeros((len(jd), len(RF_FEATURE_NAMES)), dtype=float)
+to_return_features[mask] = test_features
 
-    concatenated_features = [
-        ','.join(np.array(i, dtype=str)) for i in to_return_features
-    ]
+concatenated_features = [
+','.join(np.array(i, dtype=str)) for i in to_return_features
+]
 
-    return pd.Series(concatenated_features)
+return pd.Series(concatenated_features)
 
 
 @pandas_udf(DoubleType(), PandasUDFType.SCALAR)
-<<<<<<< HEAD
-def rfscore_sigmoid_elasticc(midPointTai, filterName, psFlux, psFluxErr,
-                             cdsxmatch, nobs, hostgal_ra, hostgal_dec,
-                             hostgal_zphot, hostgal_zphot_err,
-                             mwebv, maxduration=None,
-                             model=None) -> pd.Series:
-=======
 def rfscore_sigmoid_elasticc(
         midPointTai, filterName, psFlux, psFluxErr,
         cdsxmatch, nobs, ra, dec, hostgal_ra, hostgal_dec,
         hostgal_zphot, hostgal_zphot_err,
         mwebv, maxduration=None,
         model=None) -> pd.Series:
->>>>>>> 0684f25986c5088dba48f0da0af5e1c4c532d24e
     """ Return the probability of an alert to be a SNe Ia using a Random
     Forest Classifier (sigmoid fit) on ELaSTICC alert data.
 
@@ -346,10 +338,7 @@ def rfscore_sigmoid_elasticc(
     # Perform the fit + classification (default model)
     >>> args = [F.col(i) for i in what_prefix]
     >>> args += [F.col('cdsxmatch'), F.col('diaSource.nobs')]
-<<<<<<< HEAD
-=======
     >>> args += [F.col('diaObject.ra'), F.col('diaObject.decl')]
->>>>>>> 0684f25986c5088dba48f0da0af5e1c4c532d24e
     >>> args += [F.col('diaObject.hostgal_ra'), F.col('diaObject.hostgal_dec')]
     >>> args += [F.col('diaObject.hostgal_zphot')]
     >>> args += [F.col('diaObject.hostgal_zphot_err'), F.col('diaObject.mwebv')]
@@ -394,13 +383,6 @@ def rfscore_sigmoid_elasticc(
         feats = []
         nfeat_per_band = 6
         nbands = 6
-<<<<<<< HEAD
-        meta_feats = []
-        meta = [hostgal_ra, hostgal_dec, hostgal_zphot,
-                hostgal_zphot_err, mwebv]
-        for m in meta:
-            meta_feats.append(m)
-=======
 
         # Julien added `id`
         meta_feats = [
@@ -412,7 +394,6 @@ def rfscore_sigmoid_elasticc(
             hostgal_zphot_err.values[id],
             mwebv.values[id]
         ]
->>>>>>> 0684f25986c5088dba48f0da0af5e1c4c532d24e
 
         for i in range(nbands):
             feats.append(features[i * nfeat_per_band])
@@ -423,16 +404,7 @@ def rfscore_sigmoid_elasticc(
             flag.append(False)
         else:
             flag.append(True)
-<<<<<<< HEAD
-        test_features.append(features)
-        test_features.append(meta_feats)
-=======
         test_features.append(np.concatenate((features, meta_feats)))
-
-        # From Marco
-        # test_features.append(features)
-        # test_features.append(meta_feats)
->>>>>>> 0684f25986c5088dba48f0da0af5e1c4c532d24e
 
     flag = np.array(flag, dtype=np.bool)
 
