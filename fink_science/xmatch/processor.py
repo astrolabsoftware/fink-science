2971043e3c3a32230f8b4e5c1486b46c5f5e1709
JulienPeloton <peloton@lal.in2p3.fr>
2024-01-30 10:53:21 +0100

    Add example for SPICU

--- fink_science/xmatch/processor.py
+++ fink_science/xmatch/processor.py
@@ -233,6 +233,16 @@ def xmatch_cds(
     ...     types=['string'])
     >>> 'Type' in df_vsx.columns
     True
+
+    # SPICY
+    >>> df_spicy = xmatch_cds(
+    ...     df,
+    ...     catalogname="vizier:J/ApJS/254/33/table1",
+    ...     distmaxarcsec=1.2,
+    ...     cols_out=['SPICY', 'class'],
+    ...     types=['string', 'string'])
+    >>> 'SPICY' in df_spicys.columns
+    True
     """
     df_out = df.withColumn(
         'xmatch',
