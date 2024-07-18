5f31f9e9cedf318ab0088c100d3df2cccc4371c8
Roman <roman.lemontagner@gmail.com>
2024-07-18 09:32:14 +0200

    remove unused line for pep8

--- fink_science/tester.py
+++ fink_science/tester.py
@@ -13,7 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import sys
-from fink_science import __file__
 import doctest
 import numpy as np
 
