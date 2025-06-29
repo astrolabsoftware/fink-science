2c9569ff58f9a6bc133ec7d075c87d581ca6acec
utthishtastro <astrolearn4@gmail.com>
2025-06-29 22:18:08 +0200

    Update authors list in hostless detection powerspectrum file

--- fink_science/ztf/hostless_detection/powerspectrum.py
+++ fink_science/ztf/hostless_detection/powerspectrum.py
@@ -1,5 +1,5 @@
 # Copyright 2024 AstroLab Software
-# Author: R. Durgesh
+# Author: E. E. Hayes, R. Durgesh
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
