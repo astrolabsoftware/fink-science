b869370e1932bf69279d2cb79310f6a3f4a29361
JulienPeloton <peloton@lal.in2p3.fr>
2025-07-04 11:15:55 +0200

    Expand test suite. Fix empty batch after quality cuts.

--- fink_science/rubin/orphans/processor.py
+++ fink_science/rubin/orphans/processor.py
@@ -17,6 +17,7 @@ import pandas as pd
 import numpy as np
 
 from fink_science.rubin.orphans.classifier import orphan_classifier
+from fink_science.rubin.orphans.basic_functions import flux_to_mag
 
 from fink_science import __file__
 from fink_science.tester import spark_unit_tests
@@ -26,32 +27,36 @@ from pyspark.sql.types import FloatType
 
 
 @pandas_udf(FloatType())
-def orphan_grb(cmidpointMjdTai, cabmags, cabmagserr, cband):
+def orphan_grb(cmidpointMjdTai, cpsfFlux, cpsfFluxErr, cband):
     """Filter to extract orphan GRB candidates
 
+    Notes
+    -----
     The filter is based on different features calculated from the light curve:
     - Duration between the first detection and the peak
     - Increase and decrease rates
     - Colour between different filter pairs
     - 4 parameters and chi2 from fitting the light curve
 
-    A Boosting Decision Tree classifier was trained to discriminate orphans using these features
+    A Boosting Decision Tree classifier was trained to discriminate
+    orphans using these features
 
     Parameters
     ----------
     cmidpointMjdTai: pandas.Series of list of float
-        Concatenated time in MJD for the object
-    cabmags: pandas.Series of list of float, or Nan
-        Concatenated magnitude for the object
-    cabmagserr: pandas.Series of list of float, or Nan
-        Concatenated errors on the magnitude for the object
+        Concatenated time in MJD for alerts
+    cpsfFlux: pandas.Series of list of float, or Nan
+        Concatenated fluxes for alerts
+    cpsfFluxErr: pandas.Series of list of float, or Nan
+        Concatenated errors on the fluxes for alerts
     cband: pandas.Series of list of int
-        Concatenated filters for the object
+        Concatenated filters for alerts
 
     Returns
     -------
     out: pandas.Series of bool
-        Return a Pandas DataFrame with the proba of an event to be an orphan
+        Return a Pandas DataFrame with the probability
+        of each alert to be an orphan
 
     Examples
     --------
@@ -74,8 +79,23 @@ def orphan_grb(cmidpointMjdTai, cabmags, cabmagserr, cband):
 
     # Perform the fit + classification (default model)
     >>> args = [F.col(i) for i in what_prefix]
+    >>> df = df.withColumn("prob", orphan_grb(*args))
+
+    # No match obviously from the current test set
+    >>> df.filter(df["prob"] > 0.0).count()
+    0
     """
+    # Compute diff mag from diff flux
+    cabmags = cpsfFlux.apply(lambda x: flux_to_mag(x))
+    cabmagserr = (
+        cpsfFluxErr.apply(lambda x: np.array(x))
+        / cpsfFlux.apply(lambda x: np.array(x))
+        * 1.0857
+    )
+
     valid = cabmags.apply(lambda x: True if len(~np.isnan(x)) >= 5 else False)
+    if np.sum(valid) == 0:
+        return pd.Series(np.zeros(len(cmidpointMjdTai), dtype=float))
     proba_orphan = orphan_classifier(cmidpointMjdTai, cabmags, cabmagserr, cband, valid)
 
     return pd.Series(proba_orphan)
@@ -91,4 +111,3 @@ if __name__ == "__main__":
 
     # Run the test suite
     spark_unit_tests(globs)
-
