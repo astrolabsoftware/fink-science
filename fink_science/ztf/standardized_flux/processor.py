44dcd4757e3ba10ba6a5e37ae8d172b681639939
JulianHamo <hamo@ijclab.in2p3.fr>
2025-07-22 15:41:53 +0200

    Update CTAO blazar catalog (fixes #553) (#554)
    * Update CTAO blazar catalog (fixes #553)
    
    * Correction of test in blazar_low_test
    
    * Correction of test in standardized_flux module pt.1

--- fink_science/ztf/standardized_flux/processor.py
+++ fink_science/ztf/standardized_flux/processor.py
@@ -25,7 +25,7 @@ from fink_science.tester import spark_unit_tests
 from fink_science import __file__
 import os
 
-RELEASE = 22
+CATALOG_TAG = "23.v7_2025"
 
 
 @pandas_udf(MapType(StringType(), ArrayType(DoubleType())))
@@ -133,13 +133,13 @@ def standardized_flux(
 
     # Test
     >>> parDF.filter(F.array_max(parDF['cstd_flux']) < 1).count()
-    30
+    28
     >>> parDF.filter(F.array_max(parDF['cstd_flux']) > 1).count()
     64
     """
     path = os.path.dirname(os.path.abspath(__file__))
     CTAO_PATH = os.path.join(path, "data/catalogs")
-    CTAO_filename = "CTAO_blazars_ztf_dr{}.parquet".format(RELEASE)
+    CTAO_filename = "CTAO_blazars_ztf_dr{}.parquet".format(CATALOG_TAG)
     CTAO_blazar = pd.read_parquet(os.path.join(CTAO_PATH, CTAO_filename))
 
     pdf = pd.DataFrame({
