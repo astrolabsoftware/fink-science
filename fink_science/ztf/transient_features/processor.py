51ecf084ac0f87f895027aeba7719f9adbf6eaca
JulienPeloton <peloton@lal.in2p3.fr>
2025-09-17 09:47:49 +0200

    Update author list

--- fink_science/ztf/transient_features/processor.py
+++ fink_science/ztf/transient_features/processor.py
@@ -1,5 +1,5 @@
 # Copyright 2025 AstroLab Software
-# Author: Julien Peloton
+# Author: Julien Peloton, Etienne Russeil, Daniel Perley
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
