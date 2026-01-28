ca88814242108289455f93cd092371df286fbbdb
Julien <peloton@lal.in2p3.fr>
2026-01-28 11:32:32 +0100

    Switch from digits to letters for feature keys (#620)
    * Switch from digits to letters for feature keys
    
    * Remove unecessary comments about the mapping

--- fink_science/rubin/ad_features/test.py
+++ fink_science/rubin/ad_features/test.py
@@ -32,10 +32,12 @@ bands = np.random.choice(["g", "r"], N)
 features = extract_features_ad_rubin_raw(mjds, flux, fluxerr, bands, "TestObject1")
 
 # Basic assertions
-assert len(features) == 2, "Should have features for 2 bands (g=1, r=2)"
-assert 1 in features, "Band g (mapped to 1) missing"
-assert 2 in features, "Band r (mapped to 2) missing"
-assert len(features[1]) == len(FEATURES_COLS), "Incorrect number of features extracted"
+assert len(features) == 2, "Should have features for 2 bands (g, r)"
+assert "g" in features, "Band g missing"
+assert "r" in features, "Band r missing"
+assert len(features["g"]) == len(FEATURES_COLS), (
+    "Incorrect number of features extracted"
+)
 
 print("Test passed: Features extracted successfully.")
-print(f"Example feature (Amplitude band g): {features[1]['amplitude']}")
+print(f"Example feature (Amplitude band g): {features['g']['amplitude']}")
