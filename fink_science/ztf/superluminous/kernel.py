9f244dea8c092b24334356e3ab4f061b932401d1
JulienPeloton <peloton@lal.in2p3.fr>
2026-02-15 20:55:02 +0100

    Catch TypeError & define i-band in the SLSN kernel with a dummy value

--- fink_science/ztf/superluminous/kernel.py
+++ fink_science/ztf/superluminous/kernel.py
@@ -19,7 +19,8 @@ from fink_science import __file__
 curdir = os.path.dirname(os.path.abspath(__file__))
 
 classifier_path = curdir + "/data/models/superluminous_classifier.joblib"
-band_wave_aa = {1: 4770.0, 2: 6231.0}
+# Declare i-band with dummy value
+band_wave_aa = {1: 4770.0, 2: 6231.0, 3: 0.0}
 temperature = "sigmoid"
 bolometric = "bazin"
 min_points_total = 7
