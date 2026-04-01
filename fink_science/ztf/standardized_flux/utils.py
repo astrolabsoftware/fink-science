fa22774ea71c2e473dca055c43aa3c7616753c96
JulianHamo <hamo@ijclab.in2p3.fr>
2026-04-01 09:51:31 +0200

    Add handling of extreme states in blazar analysis (fixes #680) (#681)
    * Initial commit: blazar_extreme_state module and associated catalog + catalog production pipeline
    
    * PEP8 fix
    
    * PEP8 fix
    
    * PEP8 fix
    
    * PEP8 fix
    
    * Test fix
    
    * Fix function from different numpy versions
    
    * Fix wrong key call for high state dict
    
    * Fix key int to key str
    
    * Fix key int to key str
    
    * Fix key int to key str
    
    * Fix key int to key str
    
    * Test for standardized flux abnormal values
    
    * Test for standardized flux abnormal values
    
    * Test for standardized flux abnormal values
    
    * Fix test for standardized flux
    
    * Add more unit tests for blazar_extreme_state module
    
    * Fix additional unit tests for blazar_extreme_state module
    
    * Fix additional unit tests for blazar_extreme_state module
    
    * PR FIX: Remove comment header
    
    * PR FIX: Remove global logger
    
    * PR FIX: Remove comments outside of code
    
    * PR FIX: Remove overall try and except logic in main
    
    * PR FIX: Rewrite _from_mag_to_flux
    
    * PR FIX: Remove .copy() behaviour of _standardise_lc_1band
    
    * PR FIX: Remove .copy() behaviour of _standardise_lc_2bands
    
    * PR FIX: Remove overwriting of integration_period in _robustness_criterion
    
    * PR FIX: Remove useless escape characters in docstring of extreme_state
    
    * PR FIX: reuse functions from blazar_extreme_state
    
    * Transform relative path to absolute path for import
    
    * PR FIX: Handle errors more properly
    
    * PR FIX: Simplify error handling
    
    * PR FIX: Simplify error handling
    
    * PR FIX: Simplify error handling
    
    * PEP: Remove temporary fix
    
    * PEP: Fix E231
    
    * PEP: Fix too long lines
    
    ---------
    
    Co-authored-by: Julian Hamo <julian.hamo@ijclab.in2p3.fr>

--- fink_science/ztf/standardized_flux/utils.py
+++ fink_science/ztf/standardized_flux/utils.py
@@ -7,33 +7,33 @@ from fink_utils.photometry.conversion import apparent_flux
 def standardized_flux_(pdf: pd.DataFrame, CTAO_blazar: pd.DataFrame) -> tuple:
     """Returns the standardized flux and its uncertainties for a batch of alerts
 
-    Notes
-    -----
-    Standardized flux means flux over median of each band.
-
     Parameters
     ----------
-    pdf: pd.core.frame.DataFrame
+    pdf: pd.DataFrame
         Pandas DataFrame of the alert history containing:
         candid, ojbectId, cdistnr, cmagpsf, csigmapsf,
         cmagnr, csigmagnr, cisdiffpos, cfid, cjd
-    CTAO_blazar: pd.core.frame.DataFrame
+    CTAO_blazar : pd.DataFrame
         Pandas DataFrame of the monitored sources containing:
-        3FGL Name, ZTF Name, Arrays of Medians, Computed Threshold,
-        Observed Threshold, Redshift, Final Threshold
+        ``Source_name``, ``ZTF_name``, ``medians``,
+        ``low_threshold``, ``high_threshold``.
 
     Returns
     -------
     Tuple of pandas.Series
         Standardized flux and its uncertainties
+
+    Notes
+    -----
+    Standardized flux means flux over median of each band.
     """
     std_flux = np.full(len(pdf), np.nan)
     sigma_std_flux = np.full(len(pdf), np.nan)
 
     name = pdf["objectId"].to_numpy()[0]
-    CTAO_data = CTAO_blazar.loc[CTAO_blazar["ZTF Name"] == name]
+    CTAO_data = CTAO_blazar.loc[CTAO_blazar["ZTF_name"] == name]
     if not CTAO_data.empty:
-        flux_dc, sigma_flux_dc = 1000 * np.transpose([
+        flux_dc, sigma_flux_dc = np.transpose([
             apparent_flux(*args)
             for args in zip(
                 pdf["cmagpsf"].astype(float).to_numpy(),
@@ -47,7 +47,7 @@ def standardized_flux_(pdf: pd.DataFrame, CTAO_blazar: pd.DataFrame) -> tuple:
         # Loop over g & r only
         for filter_ in [1, 2]:
             maskFilt = pdf["cfid"] == filter_
-            median = CTAO_data["Array of Medians"].to_numpy()[0][filter_ - 1]
+            median = CTAO_data["medians"].iloc[0][str(filter_)]
             std_flux[maskFilt] = flux_dc[maskFilt] / median
             sigma_std_flux[maskFilt] = sigma_flux_dc[maskFilt] / median
         return pd.Series(std_flux), pd.Series(sigma_std_flux)
