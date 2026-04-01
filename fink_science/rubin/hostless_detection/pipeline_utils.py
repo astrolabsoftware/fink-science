a015fb45dce1ad4bf84fa28886f7e65fd182969a
utthishtastro <81102766+utthishtastro@users.noreply.github.com>
2026-04-01 09:41:28 +0200

    Filter SSOs by applying polynomial fitting to positions over time (#679)
    * Filter SSOs by applying polynomial fitting to positions over time
    
    * Remove mag cut in hostless science module

--- fink_science/rubin/hostless_detection/pipeline_utils.py
+++ fink_science/rubin/hostless_detection/pipeline_utils.py
@@ -48,7 +48,6 @@ def is_outliers_in_template(clipped_image, number_of_pixels: int = 20):
     But should be updated eventually
     """
     num_template_pixels_masked = np.ma.count_masked(clipped_image)
-    print(num_template_pixels_masked)
     if num_template_pixels_masked > number_of_pixels:
         return True
     return False
@@ -105,3 +104,73 @@ def run_hostless_detection_with_clipped_data(
         science_clipped, template_clipped, detection_config
     )
     return is_hostless_candidate
+
+
+def maybe_moving_transient(
+    ra: float,
+    dec: float,
+    midpointMjdTai: float,
+    hist_ra: np.ndarray,
+    hist_dec: np.ndarray,
+    hist_midpointMjdTai: np.ndarray,
+    min_detections: int = 3,
+    min_moving_arcsec_hour: int = 2,
+    max_rms_arcsec: float = 0.5,
+):
+    """Checks if transient is moving by fitting 1st degree polynomial with ra/dec vs time
+
+    Parameters
+    ----------
+    ra
+       RA
+    dec
+        dec
+    midpointMjdTai
+        MJD of the alert
+    hist_ra
+        previous RAs
+    hist_dec
+        previous Decs'
+    hist_midpointMjdTai
+        previous MJDs
+    min_moving_arcsec_hour
+        moving threshold
+    max_rms_arcsec
+        rms residual threshold
+    """
+    if hist_ra is None:
+        return False
+    ra_array = np.append(hist_ra, ra)
+    dec_array = np.append(hist_dec, dec)
+    midpointMjdTai_array = np.append(hist_midpointMjdTai, midpointMjdTai)
+
+    if len(ra_array) < min_detections:
+        return False
+    ra_array = np.deg2rad(ra_array)
+    dec_array = np.deg2rad(dec_array)
+
+    ra_array = np.unwrap(ra_array)
+
+    dec_mean = np.mean(dec_array)
+    ra_true = ra_array * np.cos(dec_mean)
+
+    t0 = np.mean(midpointMjdTai_array)
+    t_normalized = midpointMjdTai_array - t0
+    ra_coefficients = np.polyfit(t_normalized, ra_true, 1)
+    dec_coefficients = np.polyfit(t_normalized, dec_array, 1)
+
+    ra_fit = np.polyval(ra_coefficients, t_normalized)
+    dec_fit = np.polyval(dec_coefficients, t_normalized)
+
+    ra_residual = ra_true - ra_fit
+    dec_residual = dec_array - dec_fit
+    rms_rad = np.sqrt(np.mean(ra_residual**2 + dec_residual**2))
+    rms_arcsec = rms_rad * (180 / np.pi) * 3600
+
+    mu_rad_day = np.sqrt(ra_coefficients[0] ** 2 + dec_coefficients[0] ** 2)
+    arcsec_hour = mu_rad_day * (180 / np.pi) * 3600 / 24
+
+    is_moving_object = (
+        arcsec_hour > min_moving_arcsec_hour and rms_arcsec < max_rms_arcsec
+    )
+    return is_moving_object
