95b92e77a5f7141cbf8ffc1f2bb0cb77265b85ce
Timofei Pshenichnyy <93309519+Knispel2@users.noreply.github.com>
2026-04-15 23:08:51 +0300

    Add new filter (#691)
    * Add new filter
    
    * up count

--- fink_science/ztf/anomaly_detection/processor.py
+++ fink_science/ztf/anomaly_detection/processor.py
@@ -67,9 +67,18 @@ ANOMALY_MODELS = [
     "_emille_30days",
     "_varvara",
 ]  # noqa
-NOTIFICATIONS_COUNT = {"_emille": 30, "_emille_30days": 30, "_varvara": 100}
+NOTIFICATIONS_COUNT = {"_emille": 30, "_emille_30days": 30, "_varvara": 200}
 CURVE_LAST_DAYS = {"_emille_30days": 30}
-CUSTOM_FILTERS = {"_beta": "objectId LIKE '%ZTF21%'"}
+CUSTOM_FILTERS = {
+    "_beta": "objectId LIKE '%ZTF21%'",
+    "_varvara": (
+        "ABS("
+        "SIN(RADIANS(dec)) * SIN(RADIANS(27.12825)) + "
+        "COS(RADIANS(dec)) * COS(RADIANS(27.12825)) * "
+        "COS(RADIANS(ra - 192.85948))"
+        ") > 0.5"
+    ),
+}
 
 
 class TwoBandModel:
