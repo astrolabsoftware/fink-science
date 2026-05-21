2222eefc7dafa1f71c1a3972e840fab046a9cf2a
Julien <peloton@lal.in2p3.fr>
2026-05-21 07:31:56 +0200

    Update test suite for orphans (#696)
    * Bump to 8.46.0
    
    * Update test suite for orphans

--- fink_science/__init__.py
+++ fink_science/__init__.py
@@ -12,4 +12,4 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-__version__ = "8.45.0"
+__version__ = "8.46.0"
