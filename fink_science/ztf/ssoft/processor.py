6821b3671835ecccba8eccd20eeab1daeebbc247
Julien <peloton@lal.in2p3.fr>
2026-06-04 09:59:36 +0200

    Fix fink_tns imports (#704)
    * Fix fink_tns imports
    
    * Bump fink-tns in CI
    
    * Try without remapping

--- fink_science/ztf/ssoft/processor.py
+++ fink_science/ztf/ssoft/processor.py
@@ -633,7 +633,7 @@ def extract_ssoft_parameters(
                 bounds=MODELS[model_name]["bounds"],
                 model=model_name,
                 normalise_to_V=False,
-                remap=True,
+                remap=False,
             )
 
         # Add astrometry
