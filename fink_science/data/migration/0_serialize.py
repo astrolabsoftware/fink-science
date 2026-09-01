1747c53480540c5de65d976bf8080587ac503b08
JulienPeloton <peloton@lal.in2p3.fr>
2026-09-01 09:24:17 +0200

    Update migration scripts

--- fink_science/data/migration/0_serialize.py
+++ fink_science/data/migration/0_serialize.py
@@ -1,30 +1,44 @@
+# Copyright 2019-2026 AstroLab Software
+# Author: Julien Peloton
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import json
-import joblib
 import sklearn
 import numpy as np
 from joblib import load
 
-from sklearn.ensemble import RandomForestClassifier
 
-from sklearn_migrator.classification.random_forest_clf import serialize_random_forest_clf
+from sklearn_migrator.classification.random_forest_clf import (
+    serialize_random_forest_clf,
+)
 from sklearn_migrator.dimension.pca import serialize_pca
 
 import argparse
 
+
 def main():
     """Serialize model into a JSON-compatible dictionary"""
-    parser = argparse.ArgumentParser(description="Serialize model into a JSON-compatible dictionary")
-    parser.add_argument(
-        '-modelfn', type=str, default=None,
-        help="Model name"
+    parser = argparse.ArgumentParser(
+        description="Serialize model into a JSON-compatible dictionary"
     )
+    parser.add_argument("-modelfn", type=str, default=None, help="Model name")
     args = parser.parse_args(None)
 
     version_sklearn_in = sklearn.__version__
 
-    model = load('input_{}/'.format(version_sklearn_in) + args.modelfn)
+    model = load("input_{}/".format(version_sklearn_in) + args.modelfn)
 
-    if args.modelfn.startswith('pca'):
+    if args.modelfn.startswith("pca"):
         all_data = serialize_pca(model, version_sklearn_in)
     else:
         all_data = serialize_random_forest_clf(model, version_sklearn_in)
@@ -36,13 +50,14 @@ def main():
             return float(o)
         elif isinstance(o, np.ndarray):
             return o.tolist()
-        elif isinstance(o, NoneType):
-            return 0
         else:
-            raise TypeError(f"Object of type {type(o).__name__} is not JSON serializable")
+            raise TypeError(
+                f"Object of type {type(o).__name__} is not JSON serializable"
+            )
 
-    with open(args.modelfn.split('.')[0] + '.json', "w") as f:
+    with open(args.modelfn.split(".")[0] + ".json", "w") as f:
         json.dump(all_data, f, default=convert)
+
 
 if __name__ == "__main__":
     main()
--- fink_science/data/migration/1_xgboost.py
+++ fink_science/data/migration/1_xgboost.py
@@ -1,30 +1,52 @@
+# Copyright 2019-2026 AstroLab Software
+# Author: Julien Peloton
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 import os
 import xgboost
 import joblib
+import pickle
 
 import argparse
 
+
 def main():
     """Deserialize model into a JSON-compatible dictionary"""
-    parser = argparse.ArgumentParser(description="Serialize model into a JSON-compatible dictionary")
-    parser.add_argument(
-        '-modelfn', type=str, default=None,
-        help="Model name"
+    parser = argparse.ArgumentParser(
+        description="Serialize model into a JSON-compatible dictionary"
     )
+    parser.add_argument("-modelfn", type=str, default=None, help="Model name")
     args = parser.parse_args(None)
 
     version_xgboost_out = xgboost.__version__
 
-    model = xgboost.XGBRFClassifier()
+    model = xgboost.XGBClassifier()
     model.load_model(args.modelfn.split(".")[0] + ".json")
 
-    outfolder = 'xgboost_output_{}'.format(version_xgboost_out)
+    with open("model_attrs.pkl", "rb") as f:
+        attributes_to_keep = pickle.load(f)
+
+    for attr, val in attributes_to_keep.items():
+        setattr(model, attr, val)
+
+    outfolder = "xgboost_output_{}".format(version_xgboost_out)
     os.makedirs(outfolder, exist_ok=True)
 
-    model.save_model('{}/{}'.format(outfolder, args.modelfn.split('.')[0] + '.json'))
+    joblib.dump(model, "{}/{}".format(outfolder, args.modelfn))
+    # model.save_model('{}/{}'.format(outfolder, args.modelfn.split('.')[0] + '.json'))
 
     print(model)
 
+
 if __name__ == "__main__":
     main()
-
